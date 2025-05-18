b12c154889c8d8455c8764046be20da0099785ad
ignacioruizberenguela <ig.ruizb@duocuc.cl>
2025-05-18 15:51:15 -0400

    Update Usuario.java

--- src/main/java/com/EduTech_Innovators/Edutech/Repository/Usuario.java
+++ src/main/java/com/EduTech_Innovators/Edutech/Repository/Usuario.java
@@ -1,4 +1,5 @@
-package
-
-public interface Repository extends JpaRepository<Usuario,Long>{ 
-  
+package com.EduTech_Innovators.Edutech.Repository;
+import org.springframework.data.jpa.Repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface Usuario.java extends JpaRepository<Usuario,Long>{ 
+}
