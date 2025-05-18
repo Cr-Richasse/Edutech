4ded1d8e68ccf7fc62225b4d6a8ec6cccba027ce
ignacioruizberenguela <ig.ruizb@duocuc.cl>
2025-05-18 16:37:45 -0400

    Update admin.java

--- src/main/java/com/EduTech_Innovators/Edutech/service/admin.java
+++ src/main/java/com/EduTech_Innovators/Edutech/service/admin.java
@@ -1,6 +1,11 @@
-private UsuarioRepositorio usuario repositorio
-public adminService
-public user Actualizar(Usuario usuario)
+package com.Edutech.service.admin.java;
+import com.Edutech.model.user;
+import com.Edutech.Repository.Usuario.java;
+import java.util.list;
+
+public class adminService{
+ public adminService(
+ public user Actualizar(Usuario usuario)
   Usuario usuario=
   
 
