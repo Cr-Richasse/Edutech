d0001abc9a35d0abc13acfccf332b869ed134840
ignacioruizberenguela <ig.ruizb@duocuc.cl>
2025-05-19 16:04:35 -0400

    Update UsuarioService.java

--- src/main/java/com/EduTech_Innovators/Edutech/service/UsuarioService.java
+++ src/main/java/com/EduTech_Innovators/Edutech/service/UsuarioService.java
@@ -1,1 +1,12 @@
+package com.Edutech.service.UsuarioService.java;
+import com.Edutech.model.Usuario.java;
+import com.Edutech.repository.Usuario.java;
+import java.util.list;
 
+public class UsuarioService(){
+  private UsuarioRepository usuarioRepository;
+
+  public Usuario crearCuenta(Usuario usuarioNuevo){
+    Usuario.setEmail(usuarioNuevo.getEmail());
+    Usuario.setnombres(usuarioNuevo.getnombres());
+    
