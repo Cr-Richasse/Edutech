c505ed8a33daa60b2da529a420282ec269b4292a
Joaquin <joa.fernandezf@duocuc.cl>
2025-05-19 16:35:47 -0400

    Create UsuarioService.java

--- src/main/java/com/EduTech_Innovators/Edutech/repository/UsuarioRepository.java
+++ src/main/java/com/EduTech_Innovators/Edutech/repository/UsuarioRepository.java
@@ -1,4 +1,8 @@
-package com.EduTech_Innovators.Edutech.Repository;
-import org.springframework.data.jpa.Repository.JpaRepository
-public interface Usuario.java extends JpaRepository<Usuario,Long>{ 
+package com.EduTech_Innovators.Edutech.repository;
+import com.EduTech_Innovators.Edutech.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
 }
