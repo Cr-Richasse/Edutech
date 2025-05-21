d825800c49a890baf95190a8bd28e143ed69dd1b
Cr-Richasse <cr.richasse@duocuc.cl>
2025-05-21 19:12:21 -0400

    Service
    la adicion de GerenteClasesService y la actualizacion de ProfesorService

---
+++ src/main/java/com/EduTech_Innovators/Edutech/service/GerenteClasesService.java
@@ -0,0 +1,47 @@
+package com.EduTech_Innovators.Edutech.service;
+
+import com.EduTech_Innovators.Edutech.model.GerenteClases;
+import com.EduTech_Innovators.Edutech.repository.GerenteClasesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GerenteClasesService {
+    @Autowired
+
+    private final GerenteClasesRepository gerenteClasesRepository;
+
+    public GerenteClasesService(GerenteClasesRepository gerenteClasesRepository) {
+        this.gerenteClasesRepository = gerenteClasesRepository;
+    }
+    public String crearGerente(GerenteClases gerente) {
+        gerenteClasesRepository.save(gerente);
+        return "se creó con exito";
+    }
+    public List<GerenteClases> obtenerGerente(){
+        return gerenteClasesRepository.findAll();
+    }
+    public GerenteClases obtenerGerente(int id){
+        return gerenteClasesRepository.findById(id).orElse(null);
+    }
+    public GerenteClases actuGerente(GerenteClases gerente){
+        GerenteClases gerenteClases = gerenteClasesRepository.findById(gerente.getId()).orElse(null);
+        if(gerenteClases != null){
+            gerenteClases.setEmail(gerente.getEmail());
+            gerenteClases.setNombres(gerente.getNombres());
+            gerenteClases.setApellidoPaterno(gerente.getApellidoPaterno());
+            gerenteClases.setApellidoMaterno(gerente.getApellidoMaterno());
+            gerenteClases.setNombreUsuario(gerente.getNombreUsuario());
+            gerenteClases.setAcceso(gerente.getAcceso());
+            return gerenteClasesRepository.save(gerenteClases);
+        }
+        return null;
+    }
+    public String eliminarGerente(int id){
+        gerenteClasesRepository.deleteById(id);
+        return "Se elimino con exito";
+    }
+
+}
--- src/main/java/com/EduTech_Innovators/Edutech/service/ProfesorService.java
+++ src/main/java/com/EduTech_Innovators/Edutech/service/ProfesorService.java
@@ -15,16 +15,32 @@ public class ProfesorService {
     public ProfesorService(ProfesorRepository profesorRepository) {
         this.profesorRepository = profesorRepository;
     }
+    public String crearProfesor(Profesor profesor) {
+        profesorRepository.save(profesor);
+        return "Profesor creado";
+    }
     public List<Profesor> getProfesores() {
         return profesorRepository.findAll();
     }
-    public Profesor createProfesor(Profesor profesor) {
-        Profesor prof = new Profesor();
-        prof.setEmail(profesor.getEmail());
-        prof.setNombres(profesor.getNombres());
-        prof.setApellidoPaterno(profesor.getApellidoPaterno());
-        prof.setApellidoMaterno(profesor.getApellidoMaterno());
-        return profesorRepository.save(prof);
+    public Profesor getProfesor(int id) {
+        return profesorRepository.findById(id).orElse(null);
+    }
+    public Profesor actProfesor(Profesor profesor) {
+        Profesor prof = profesorRepository.findById(profesor.getId()).orElse(null);
+        if (prof!= null) {
+            prof.setEmail(profesor.getEmail());
+            prof.setNombres(profesor.getNombres());
+            prof.setApellidoPaterno(profesor.getApellidoPaterno());
+            prof.setApellidoMaterno(profesor.getApellidoMaterno());
+            prof.setNombreUsuario(profesor.getNombreUsuario());
+            prof.setAcceso(profesor.getAcceso());
+            return profesorRepository.save(prof);
+        }
+        return null;
+    }
+    public String deleteProfesor(int id) {
+        profesorRepository.deleteById(id);
+        return "Profesor eliminado";
     }
 
 }
