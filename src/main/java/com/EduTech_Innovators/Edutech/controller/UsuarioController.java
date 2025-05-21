c1753d6bb51482de80d51adb0728dcd1713f4c34
ignacioruizberenguela <ig.ruizb@duocuc.cl>
2025-05-21 18:28:33 -0400

    Update UsuarioController.java

--- src/main/java/com/EduTech_Innovators/Edutech/controller/UsuarioController.java
+++ src/main/java/com/EduTech_Innovators/Edutech/controller/UsuarioController.java
@@ -1,1 +1,7 @@
+package com.EduTech_Innovators.Edutech.controller;
 
+import com.EduTech_Innovators.Edutech.model.Usuario;
+import com.EduTech_Innovators.Edutech.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
