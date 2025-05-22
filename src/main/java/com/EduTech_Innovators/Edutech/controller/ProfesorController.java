1e11d2caabbf4d721359d0b1264fbe51c7479c14
Cr-Richasse <cr.richasse@duocuc.cl>
2025-05-22 19:59:48 -0400

    Update
    Implementacion completa de ProfesorController

--- src/main/java/com/EduTech_Innovators/Edutech/controller/ProfesorController.java
+++ src/main/java/com/EduTech_Innovators/Edutech/controller/ProfesorController.java
@@ -1,10 +1,34 @@
 package com.EduTech_Innovators.Edutech.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.EduTech_Innovators.Edutech.model.Profesor;
+import com.EduTech_Innovators.Edutech.service.ProfesorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/profesor")
 public class ProfesorController {
-    
+
+    @Autowired
+    private ProfesorService profesorService;
+
+    @GetMapping
+    public List<Profesor> getProfesores() {return profesorService.getProfesores();}
+
+    @GetMapping("/{id}")
+    public Profesor obtenerProfesor(@PathVariable int id) {return profesorService.getProfesor(id);}
+
+    @PostMapping
+    public String crearProfesor(@RequestBody Profesor profesor) {return profesorService.crearProfesor(profesor);}
+
+    @DeleteMapping("/{id}")
+    public String eliminarProfesor(@PathVariable int id) {return profesorService.deleteProfesor(id);}
+
+    @PutMapping("/{id}")
+    public String actProfesor(@PathVariable int id, @RequestBody Profesor profesor) {
+        profesorService.actProfesor(id, profesor);
+        return"Actualizado con exito";
+    }
 }
