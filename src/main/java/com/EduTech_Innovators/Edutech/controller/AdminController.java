0e67865eab77aa1b32b289bec51898d83418215b
ignacioruizberenguela <ig.ruizb@duocuc.cl>
2025-05-22 18:46:25 -0400

    Update AdminController.java

--- src/main/java/com/EduTech_Innovators/Edutech/controller/AdminController.java
+++ src/main/java/com/EduTech_Innovators/Edutech/controller/AdminController.java
@@ -1,1 +1,13 @@
+package com.EduTech_Innovators.Edutech.controller;
 
+import com.EduTech_Innovators.Edutech.model.Admin;
+import com.EduTech_Innovators.Edutech.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+public class AdminController{
+  @Autowired
+  private AdminService adminService;
+  @GetMapping
+  
